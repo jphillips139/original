ddd8db9d6e67e83342d7a86ee9f4f89823054544
Marshall Bargar <mbargar@selectsires.com>
2016-02-06 18:32:25 +0000

    Changes made by Marshall Bargar (mbargar@selectsires.com).

--- lli/app/app.js
+++ lli/app/app.js
@@ -1,8 +1,52 @@
-var application = require('application');
+var application = require("application");
+application.mainModule = "./views/homepage";
 
-application.mainModule = 'navigation/navigation';
-// START_CUSTOM_CODE_nativeScriptApp
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+/*
+ * Define constants which we will use across the application
+ */
+global.BS_API_KEY = "zrep1x3zwr1h2s2a";
+global.BS_SCHEME = "http";
+global.BS_URL = "";
+global.TOKEN_DATA_KEY = "authenticationToken";
+global.USER_ID = "userId";
+global.EVERLIVE = null;
+global.MONITOR = null;
 
-// END_CUSTOM_CODE_nativeScriptApp
+application.onLaunch = function (context) {
+    // For Android applications, the context is an android.content.Intent class.
+    // For iOS applications, the context is undefined.
+    if (application.android) {
+        console.log("Launched Android application with the following intent: " + context + ".");
+    }
+    else if (application.ios) {
+        console.log("Launched iOS application.");
+        // Workaround for a bug in the iOS runtime version 1.2.2, should be fixed with next version
+        // This constant is used in the location module but must be referenced earlier on so that the appropriate iOS framework can be loaded
+        kCLDistanceFilterNone;
+    }
+};
+
+application.onSuspend = function () {
+    console.log("Application suspended.");
+};
+
+application.onResume = function () {
+    console.log("Application resumed.");
+};
+
+application.onExit = function () {
+    console.log("Application will exit.");
+    if(MONITOR !== null){
+        MONITOR.stop();
+    }
+};
+
+application.onLowMemory = function () {
+    console.log("Memory is low.");
+};
+
+application.onUncaughtError = function (error) {
+    console.log("Application error: " + error.name + "; " + error.message + "; " + error.nativeError);
+};
+
 application.start();
